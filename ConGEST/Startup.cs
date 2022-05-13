29c68fb9891ccb0e7aa20d69672dc10695b3441f
AntoineRondelet42 <antoine.rondelet@outlook.fr>
2022-05-13 13:55:30 +0200

    hosting on azure

--- ConGEST/Startup.cs
+++ ConGEST/Startup.cs
@@ -33,7 +33,8 @@ namespace ConGEST
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CongestContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<CongestContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<CongestContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
 
             //services.AddScoped<ISubjectRepository, SubjectRepository>();
             services.AddControllers();
