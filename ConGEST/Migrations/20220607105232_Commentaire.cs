23be046e09b7187f9c783a251807e17e7ebcef43
AntoineRondelet42 <antoine.rondelet@outlook.fr>
2022-06-07 12:53:09 +0200

    test

---
+++ ConGEST/Migrations/20220607105232_Commentaire.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConGEST.Migrations
+{
+    public partial class Commentaire : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
